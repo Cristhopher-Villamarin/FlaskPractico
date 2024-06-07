835704ff923b36336ff5e3f722d59ea6ec225afd
Cristhopher-Villamarin <87491312+Cristhopher-Villamarin@users.noreply.github.com>
2024-06-06 20:00:58 -0500

    Actualizacion Flask

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
 import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    my_set = {"Hello World from Railway whit Flask!"}
-    return jsonify(list(my_set))
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
